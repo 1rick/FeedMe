e29b3f5aafa7ddfa1e097d49abedc5922a9d4e6f
Rick Martin <ineedrick@gmail.com>
2011-03-05 22:49:30 +0900

    Added Simple Pie to parse feeds. Works locally for me, but for some reason does not work on my live server. Will play some more on this branch..

--- feeds.php
+++ feeds.php
@@ -10,6 +10,9 @@ $yahoonews = "http://rds.yahoo.com/_ylt=A2KLUmAuKttMzGEBY7DQtDMD/SIG=12pi6hu52/E
 
 $twitter = "http://search.twitter.com/search.atom?q=+REPLACEME";
 
+$reddit = "http://www.reddit.com/r/REPLACEME/.rss";
+$redditrecent = "http://www.reddit.com/r/REPLACEME/new.rss";
+
 $youtubetagged = "http://www.youtube.com/rss/tag/REPLACEME.rss";
 $youtubesearch = "http://gdata.youtube.com/feeds/api/videos?orderby=updated&vq=REPLACEME";
 
@@ -55,6 +58,9 @@ $yahoonewsnew = str_replace("REPLACEME", $foryahoofeed, $yahoonews);
 
 $twitternew = str_replace("REPLACEME", $withplussign, $twitter);
 
+$redditnew = str_replace("REPLACEME", $withplussign, $reddit);
+$redditrecentnew = str_replace("REPLACEME", $withplussign, $redditrecent);
+
 $youtubenew = str_replace("REPLACEME", $withplussign, $youtubetagged);
 $youtubesearchnew = str_replace("REPLACEME", $withplussign, $youtubesearch);
 
@@ -65,18 +71,64 @@ $youdaonew = str_replace("REPLACEME", $withplussign, $youdao);
 ?>
 
 
+
+<?php
+	//get simple pie library
+	require_once("inc/simplepie.inc");
+	
+	//new simple pie. Not parsing all feeds, but ones likely to be most useful 
+	$feed = new SimplePie();
+	
+	 $feed->set_feed_url(array(
+	    	"$deliciousnew",
+			"$googlenewsnew",
+	    	"$yahoonewsnew",
+			"$twitternew",
+	    	"$redditnew",
+			"$youtubesearchnew",
+	    	"$hatenanew",
+			"$youdaonew",
+	    ));
+
+
+	   //enable caching
+	    $feed->enable_cache(true);
+
+	    //provide the caching folder
+	    $feed->set_cache_location('cache');
+
+	    //set the amount of seconds you want to cache the feed
+	    $feed->set_cache_duration(1800);
+
+	    //init the process
+	    $feed->init();
+
+	    //let simplepie handle the content type (atom, RSS...)
+	    $feed->handle_content_type();
+
+	?>
+
+
+
+
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>RSS feed results</title>
+
+<meta http-equiv="content-type" 
+		content="text/html;charset=utf-8" />
+		
+	
 </head>
 
 
 <body>
 <h1>Feed results for "<?php echo $searchterm1; ?> <?php echo $searchterm2; ?>"</h1>
 
-<p><a href="/FeedMe/index.html">Search again</a></p>
+<p><a href="/feedme/index.html">Search again</a></p>
 <p>Right click and copy the RSS link, and paste it into your favorite reader.</p>
 
 <ul>
@@ -87,7 +139,11 @@ $youdaonew = str_replace("REPLACEME", $withplussign, $youdao);
 
 <li>Yahoo News feed: <a href="<?php echo $yahoonewsnew ; ?>" title="Yahoo News feed for <?php echo $foryahoofeed; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Yahoo News feed for <?php echo $foryahoofeed; ?>" /></a></li>
 
-<li>Twitter search eed: <a href="<?php echo $twitternew ; ?>" title="Twitter search feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Twitter search feed for <?php echo $withplussign; ?>" /></a></li>
+<li>Twitter search feed: <a href="<?php echo $twitternew ; ?>" title="Twitter search feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Twitter search feed for <?php echo $withplussign; ?>" /></a></li>
+
+<li>Reddit feed (for a subreddit): <a href="<?php echo $redditnew ; ?>" title="Reddit feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Twitter search feed for <?php echo $withplussign; ?>" /></a></li>
+<li>Newly added to subreddit: <a href="<?php echo $redditrecentnew ; ?>" title="Reddit feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Twitter search feed for <?php echo $withplussign; ?>" /></a></li>
+
 
 <li>YouTube tag feed: <a href="<?php echo $youtubenew ; ?>" title="YouTube feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="YouTube feed for <?php echo $withplussign; ?>" /></a></li>
 <li>YouTube search feed: <a href="<?php echo $youtubesearchnew ; ?>" title="YouTube feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="YouTube feed for <?php echo $withplussign; ?>" /></a></li>
@@ -96,5 +152,27 @@ $youdaonew = str_replace("REPLACEME", $withplussign, $youdao);
 
 <li>Youdao news feed: <a href="<?php echo $youdaonew ; ?>" title="Youdao feed for <?php echo $withplussign; ?>"><img src="http://farm5.static.flickr.com/4010/5164937189_b1b1e90d27_t.jpg" alt="Youdao feed for <?php echo $withplussign; ?>" /></a></li>
 </ul>
+<hr>
+
+	<h2>Parse feed items:</h2>
+			<p>Most recent first</p>
+    <?php if ($feed->error): ?>
+	  <p><?php echo $feed->error; ?></p>
+	<?php endif; ?>
+
+	<?php foreach ($feed->get_items() as $item): ?>
+
+		<div class="chunk">
+ 
+			<h4 style="background:url(<?php $feed = $item->get_feed(); echo $feed->get_favicon(); ?>) no-repeat; text-indent: 25px; margin: 0 0 10px;"><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h4>
+  
+			<p>Source: <a href="<?php $feed = $item->get_feed(); echo $feed->get_permalink(); ?>"><?php $feed = $item->get_feed(); echo $feed->get_title(); ?></a> | <?php echo $item->get_date('j M Y | g:i a T'); ?></p>
+			     
+		</div>
+
+	<?php endforeach; ?>
+
+
+
 </body>
 </html>
